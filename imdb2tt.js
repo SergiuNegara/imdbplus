3781d5180d62ff9aab1da52d61ffb66353c1d084
Sergiu Negara <negara.sergiu@gmail.com>
2012-08-17 02:02:44 +0300

    second version (16.08.2012)

--- imdb2tt.js
+++ imdb2tt.js
@@ -1,20 +1,27 @@
- // @description Adds a button to search the viewed movie on torrentsmd.com
- // @namespace   http://n-e-s.info/
- // @include     http://www.imdb.com/title/tt*
- // @require https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
- // @version 19.05.2012
- // ==/UserScript==
- 
- 
-movieId = location.pathname.match(/([0-9]){7}/)[0];
- 
+// ==UserScript==
+// @name        IMDB to TMD
+// @description Adds a button to search the viewed movie on torrentsmd.com
+// @namespace   http://n-e-s.info/
+// @include     http://www.imdb.com/title/tt*
+// @require     http://code.jquery.com/jquery-latest.min.js
+// @version     16.08.2012
+// ==/UserScript==
+
+
+movieId = location.pathname.match(/([0-9]{7})/g);
+movieName = $('title').text();
+
 if(location.pathname.match(/combined/))
 {
-  tmdurl = '<a class="linkasbutton-secondary" href="http://torrentsmd.com/browse.php?imdb=' + movieId + '" target="_blank"><img alt="On TMD" src="http://torrentsmd.com/favicon.ico" /></a>';
+  tmdurl = '<a class="linkasbutton-secondary" href="http://torrentsmd.com/browse.php?imdb=' + movieId + '" target="_blank" title="On TMD"><img alt="On TMD" src="http://torrentsmd.com/favicon.ico" /></a>';
+  ruturl = '<a class="linkasbutton-secondary" href="http://rutracker.org/forum/tracker.php?nm=' + movieName + '" target="_blank" title="On Rutracker"><img alt="On Rutracker" src="http://static.rutracker.org/favicon.ico" /></a>'
   $('#action-box').append('<br/>' + tmdurl);
+  $('#action-box').append(ruturl);
 }
 else
 {
-  tmdurl = '<a class="btn2 btn2_text_on large title-trailer" href="http://torrentsmd.com/browse.php?imdb=' + movieId + '" target="_blank"><span class="btn2_text"><img alt="On TMD" src="http://torrentsmd.com/favicon.ico" /></span></a>';
+  tmdurl = '<a class="btn2 btn2_text_on large title-trailer" href="http://torrentsmd.com/browse.php?imdb=' + movieId + '" target="_blank" title="On TMD"><span class="btn2_text"><img alt="On TMD" src="http://torrentsmd.com/favicon.ico" /></span></a>';
+  ruturl = '<a class="btn2 btn2_text_on large title-trailer" href="http://rutracker.org/forum/tracker.php?nm=' + movieName + '" target="_blank" title="On Rutracker"><span class="btn2_text"><img alt="On Rutracker" src="http://static.rutracker.org/favicon.ico" /></span></a>';
   $('#overview-bottom').append(tmdurl);
+  $('#overview-bottom').append(ruturl);
 }
