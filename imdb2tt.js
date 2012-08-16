70f8c8b709a29a88b2209206c5420d6194923c9d
Sergiu Negara <negara.sergiu@gmail.com>
2012-08-17 02:04:08 +0300

    version 1.1.01 (bug fix + minor change)

--- imdb2tt.js
+++ imdb2tt.js
@@ -1,27 +1,30 @@
 // ==UserScript==
-// @name        IMDB to TMD
+// @name        IMDB2TT
 // @description Adds a button to search the viewed movie on torrentsmd.com
 // @namespace   http://n-e-s.info/
 // @include     http://www.imdb.com/title/tt*
 // @require     http://code.jquery.com/jquery-latest.min.js
-// @version     16.08.2012
+// @version     1.1.01
 // ==/UserScript==
 
 
-movieId = location.pathname.match(/([0-9]{7})/g);
-movieName = $('title').text();
-
-if(location.pathname.match(/combined/))
-{
-  tmdurl = '<a class="linkasbutton-secondary" href="http://torrentsmd.com/browse.php?imdb=' + movieId + '" target="_blank" title="On TMD"><img alt="On TMD" src="http://torrentsmd.com/favicon.ico" /></a>';
-  ruturl = '<a class="linkasbutton-secondary" href="http://rutracker.org/forum/tracker.php?nm=' + movieName + '" target="_blank" title="On Rutracker"><img alt="On Rutracker" src="http://static.rutracker.org/favicon.ico" /></a>'
-  $('#action-box').append('<br/>' + tmdurl);
-  $('#action-box').append(ruturl);
-}
-else
-{
-  tmdurl = '<a class="btn2 btn2_text_on large title-trailer" href="http://torrentsmd.com/browse.php?imdb=' + movieId + '" target="_blank" title="On TMD"><span class="btn2_text"><img alt="On TMD" src="http://torrentsmd.com/favicon.ico" /></span></a>';
-  ruturl = '<a class="btn2 btn2_text_on large title-trailer" href="http://rutracker.org/forum/tracker.php?nm=' + movieName + '" target="_blank" title="On Rutracker"><span class="btn2_text"><img alt="On Rutracker" src="http://static.rutracker.org/favicon.ico" /></span></a>';
-  $('#overview-bottom').append(tmdurl);
-  $('#overview-bottom').append(ruturl);
-}
+$(document).ready(function(){
+  if(location.pathname.match(/combined/))
+  {
+    movieId = location.pathname.match(/([0-9]{7})/g);
+    movieName = $('title').text();
+    tmdurl = '<a class="linkasbutton-secondary" href="http://torrentsmd.com/browse.php?imdb=' + movieId + '" target="_blank" title="On TMD"><img alt="On TMD" src="http://torrentsmd.com/favicon.ico" /></a>';
+    ruturl = '<a class="linkasbutton-secondary" href="http://rutracker.org/forum/tracker.php?nm=' + movieName + '" target="_blank" title="On Rutracker"><img alt="On Rutracker" src="http://static.rutracker.org/favicon.ico" /></a>'
+    $('#action-box').append('<br/>' + tmdurl);
+    $('#action-box').append(ruturl);
+  }
+  else
+  {
+    movieId = location.pathname.match(/([0-9]{7})/g);
+    movieName = $('title').text().replace(' - IMDb', '');
+    tmdurl = '<a class="btn2 btn2_text_on large title-trailer" href="http://torrentsmd.com/browse.php?imdb=' + movieId + '" target="_blank" title="On TMD"><span class="btn2_text"><img alt="On TMD" src="http://torrentsmd.com/favicon.ico" /></span></a>';
+    ruturl = '<a class="btn2 btn2_text_on large title-trailer" href="http://rutracker.org/forum/tracker.php?nm=' + movieName + '" target="_blank" title="On Rutracker"><span class="btn2_text"><img alt="On Rutracker" src="http://static.rutracker.org/favicon.ico" /></span></a>';
+    $('#overview-bottom').append(tmdurl);
+    $('#overview-bottom').append(ruturl);
+  }
+});
