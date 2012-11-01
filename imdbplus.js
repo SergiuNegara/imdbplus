e27cb5ea850acfeefef199430c32d7872c8fad4a
Sergiu Negara <negara.sergiu@gmail.com>
2012-11-01 17:15:33 +0200

    version 2.3.0 code refactored jQuery 1.8.2 functions added new features: + wikipedia + opensubtitles + subscene

--- imdbplus.js
+++ imdbplus.js
@@ -3,30 +3,51 @@
 // @description Adds links to external sources of the same content, includind trailers on youtube, movie on kinopoisk, torrents on torrentsmd/rutracker
 // @namespace   http://n-e-s.info/
 // @include     http://www.imdb.com/title/tt*
-// @require     http://code.jquery.com/jquery-latest.min.js
-// @version     2.0.2
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
+// @version     2.3.0
 // ==/UserScript==
 
 $(document).ready(function(){
-  var m   = {};
-  m.id    = location.pathname.match(/([0-9]{7})/g)[0]; // movie id
-  m.name  = escape((location.pathname.match(/combined/)) ? $('title').text().replace(' (TV)', '') : $('#ratingWidget p strong').text()); // movie name
+  var m  = {};
+  m.Id   = getMovieId();
+  m.Tt   = getMovieTt()
 
-  var l = {}; // here goes the config for all features
-  // l.ex  = ["name", "link url", "link title", "image url", "image text"];
-  l.tmd = ["Torrents.md", "http://torrentsmd.com/browse.php?imdb=" + m.id, "On TMD", "http://torrentsmd.com/favicon.ico", "On TMD"];
-  l.rut = ["Rutracker", "http://rutracker.org/forum/tracker.php?nm=" + m.name, "On Rutracker", "http://static.rutracker.org/favicon.ico", "On Rutracker"];
-  l.yt  = ["Youtube", "https://www.youtube.com/results?search_query=" + m.name + " official trailer", "Trailer on Youtube", "https://s.ytimg.com/yt/favicon-vfldLzJxy.ico", "Trailer on Youtube"];
-  l.kp  = ["Kinopoisk", "http://www.kinopoisk.ru/index.php?first=yes&kp_query=" + m.name, "On Kinopoisk", "http://kinsburg.ru/images/kinopoisk-icon.png", "On Kinopoisk"];
+  var l = {};
+  // l.ex  = ["name", "link url", "title or alt text", "image url"];
+  l.tmd  = ["Torrents.md", "http://torrentsmd.com/browse.php?imdb=" + m.Id, "On TMD", "http://torrentsmd.com/favicon.ico"];
+  l.rut  = ["Rutracker", "http://rutracker.org/forum/tracker.php?nm=" + m.Tt, "On Rutracker", "http://static.rutracker.org/favicon.ico"];
+  l.yt   = ["Youtube", "https://www.youtube.com/results?search_query=" + m.Tt + " official trailer", "Trailer on Youtube", "https://s.ytimg.com/yt/favicon-vfldLzJxy.ico"];
+  l.kp   = ["Kinopoisk", "http://www.kinopoisk.ru/index.php?first=yes&kp_query=" + m.Tt, "On Kinopoisk", "http://kinsburg.ru/images/kinopoisk-icon.png"];
+  l.wiki = ["Wikipedia", "http://en.wikipedia.org/wiki/Special:Search?search=" + m.Tt, "Wiki", "http://en.wikipedia.org/favicon.ico"];
+  l.osub = ["OpenSubtitles", "http://www.opensubtitles.org/en/search/sublanguageid-all/imdbid-" + m.Id, "Subs on OpenSubtitles", "http://static.opensubtitles.org/favicon.ico"];
+  l.ssc  = ["Subscene", "http://subscene.com/s.aspx?q=" + m.Tt, "Subs on Subscene", "http://subscene.com/favicon.ico"];
 
-  var h = '<div id="imdbplus"><hr><h4>IMDB+ Features:</h4>';
-  for (var i in l)
-  {
-    h += '<a class="imdbplus-button linkasbutton-secondary" href="' + l[i][1] + '" target="_blank" title="' + l[i][2] + '"><img alt="' + l[i][4] + '" src="' + l[i][3] + '" /></a>'; // add each features from config
+
+  stylize(); // add script styles
+  imdbplus(); // do main action - add features to page
+
+  // Functions
+  function getMovieTt() {
+    var title = document.title.match(/^(.*) \(/)[1];
+    return title;
   }
-  h += '</div>';
-  $((location.pathname.match(/combined/)) ? '#action-box' : '#title-overview-widget').append(h);
 
-  var s = "<style>#title-overview-widget #imdbplus{padding: 5px 0 0 230px;} #title-overview-widget #imdbplus a{margin: 5px 1px;}</style>"; // css styles
-  $("head").append(s);
+  function getMovieId() {
+    var id = location.pathname.match(/title\/tt(.*?)\//i)[1];
+    return id;
+  }
+
+  function stylize() {
+    var s = "#title-overview-widget #imdbplus{padding: 5px 0 0 230px;} #title-overview-widget #imdbplus a{margin: 5px 1px;}";
+    $("head").append("<style>" + s + "</style>");
+  }
+
+  function imdbplus() {
+    var h = '<div id="imdbplus"><hr><h4>IMDB+ Features:</h4>';
+    for (var i in l) {
+      h += '<a class="imdbplus-button linkasbutton-secondary" href="' + l[i][1] + '" target="_blank" title="' + l[i][2] + '"><img alt="' + l[i][2] + '" src="' + l[i][3] + '" /></a>'; // add each feature from config
+    }
+    h += '</div>';
+    $((location.pathname.match(/combined/)) ? '#action-box' : '#title-overview-widget').append(h);
+  }
 });
